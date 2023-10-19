281649af335cf7a13c1bd5e6532ffc1534470112
victor-pagnozi <victor.pagnozi@gmail.com>
2023-10-18 22:34:11 -0300

    Done table to list products

--- projects/admin-web/src/shared/components/ItemsList/index.tsx
+++ projects/admin-web/src/shared/components/ItemsList/index.tsx
@@ -1,16 +1,44 @@
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, FileEdit, Trash2 } from "lucide-react";
 import "./style.css";
 
 export const ItemsList = () => {
   return (
-    <section id="items-list">
-      <div>
-        <ChevronDown />
-        <h2>PIZZA</h2>
-        <ChevronDown />
-      </div>
+    <>
+      <section id="items-list">
+        <div>
+          <ChevronDown />
+          <h2>PIZZA</h2>
+          <ChevronDown />
+        </div>
+      </section>
 
-      <div className="items"></div>
-    </section>
+      <div className="menu-items">
+        <table>
+          <thead>
+            <tr>
+              <td>NOME</td>
+              <td>COMPONENTES</td>
+              <td className="value-column">VALOR R$</td>
+              <td>EDITAR</td>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td colSpan={4}></td>
+            </tr>
+            <tr>
+              <td>PORTUGUESA</td>
+              <td>MOLHO DE TOMATE, QUEIJO MUSSARELA, PRESUNTO, ERVILHAS</td>
+              <td>R$ 49.90</td>
+              <td>
+                <FileEdit />
+                <Trash2 />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };

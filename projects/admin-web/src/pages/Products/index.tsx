23d93595d3e2004261548f8affe5ce8958a72c2c
victor-pagnozi <victor.pagnozi@gmail.com>
2023-10-19 21:02:24 -0300

    Done list of products page

--- projects/admin-web/src/pages/Products/index.tsx
+++ projects/admin-web/src/pages/Products/index.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import { ItemsList } from "../../shared/components/ItemsList";
-import './style.css'
+import "./style.css";
+import { ButtonSubmit } from "../../shared/components/ButtonSubmit";
+import { Link } from "react-router-dom";
 
 export const ProductsPage = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const productsAsString = window.localStorage.getItem("products") ?? "[]";
+
+    setProducts(JSON.parse(productsAsString) ?? []);
+  }, []);
+
   return (
     <section id="products-list">
-      <ItemsList />
+      <ItemsList products={products} />
+
+      <div className="actions-products-list">
+        <Link to={"/create"} className="actions-products-list-button">
+          <ButtonSubmit name="CRIAR UM NOVO PRODUTO" />
+        </Link>
+      </div>
     </section>
   );
 };
--- projects/admin-web/src/shared/components/ItemsList/index.tsx
+++ projects/admin-web/src/shared/components/ItemsList/index.tsx
@@ -1,10 +1,13 @@
 import { ChevronDown, FileEdit, Trash2 } from "lucide-react";
 import "./style.css";
+import { useState } from "react";
 
-export const ItemsList = () => {
+export const ItemsList = ({ products }: { products: any[] }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <section id="items-list">
+      <section id="items-list" onClick={() => setIsOpen(!isOpen)}>
         <div>
           <ChevronDown />
           <h2>PIZZA</h2>
@@ -12,33 +15,37 @@ export const ItemsList = () => {
         </div>
       </section>
 
-      <div className="menu-items">
-        <table>
-          <thead>
-            <tr>
-              <td>NOME</td>
-              <td>COMPONENTES</td>
-              <td className="value-column">VALOR R$</td>
-              <td>EDITAR</td>
-            </tr>
-          </thead>
+      {isOpen && (
+        <div className="menu-items">
+          <table>
+            <thead>
+              <tr>
+                <td>NOME</td>
+                <td>COMPONENTES</td>
+                <td className="value-column">VALOR R$</td>
+                <td>EDITAR</td>
+              </tr>
+            </thead>
 
-          <tbody>
-            <tr>
-              <td colSpan={4}></td>
-            </tr>
-            <tr>
-              <td>PORTUGUESA</td>
-              <td>MOLHO DE TOMATE, QUEIJO MUSSARELA, PRESUNTO, ERVILHAS</td>
-              <td>R$ 49.90</td>
-              <td>
-                <FileEdit />
-                <Trash2 />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+            <tbody>
+              <tr>
+                <td colSpan={4}></td>
+              </tr>
+              {products.map((item) => (
+                <tr>
+                  <td>{item.name}</td>
+                  <td>{item.description}</td>
+                  <td>R$ {(item.price / 100).toFixed(2)}</td>
+                  <td className="table-actions">
+                    <FileEdit />
+                    <Trash2 color="red" />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </>
   );
 };
